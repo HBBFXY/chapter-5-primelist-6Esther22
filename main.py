726a6765df78823c4751c826567a738f7069282d
6Esther22 <2917038286@qq.com>
2025-10-19 19:35:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import math
 def PrimeList(N):
-  primes=[]
-  for num in range(2,N):
-    is_prime=True
-    for i in range(2,int(math.sqrt(num)+1)):
-      if num%i==0:
-        is_prime=False
-        break
-    if is_prime:
-      primes.append(str(num))
-  return ' '.join(primes)
+    primes=[]
+    for num in range(2,N):
+        is_prime=True
+        for i in range(2,num//2+1):
+            if num%i==0:
+                is_prime=False
+                break
+        if is_prime:
+            primes.append(str(num))
+    return ' '.join(primes)
